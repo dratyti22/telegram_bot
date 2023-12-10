35fbf68de252eef29b117a43218ec4306018ed11
dratyti22 <137050298+dratyti22@users.noreply.github.com>
2023-12-10 17:05:39 +0300

    этот файл используется для описания состаяния

---
+++ core/handlers/state.py
@@ -0,0 +1,21 @@
+from aiogram.fsm.state import State, StatesGroup
+
+
+class ProductsState(StatesGroup):
+    MENU = State()
+    FURTHER = State()
+    NEXT = State()
+
+
+class TOPUPYOURBALANCESTATE(StatesGroup):
+    NUMBER = State()
+    PAYMENTSYSTEM = State()
+
+
+class AddProductsAdminState(StatesGroup):
+    PRICE = State()
+    NAME = State()
+
+
+class DeletedProductsAdminState(StatesGroup):
+    NUMBER = State()
